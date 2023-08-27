bb9042f3589f6a8c196e2f83ee9cc49fdf540d30
CC007 <Coolcat_the_best2@Hotmail.com>
2023-08-27 23:24:03 +0200

    Added demo for using parameters on the manifold-props annotations

---
+++ src/main/java/com/github/cc007/Demo7.java
@@ -0,0 +1,43 @@
+package com.github.cc007;
+
+import lombok.ToString;
+import manifold.ext.props.rt.api.set;
+import manifold.ext.props.rt.api.var;
+
+import static manifold.ext.props.rt.api.PropOption.Package;
+import static manifold.ext.props.rt.api.PropOption.Private;
+import static manifold.ext.props.rt.api.PropOption.Protected;
+
+/**
+ * With manifold-props, you can also specify if a setter should be private or protected.
+ */
+public class Demo7 {
+
+    @ToString
+    public static class Properties {
+        // public modifier needed here, see issue: https://github.com/manifold-systems/manifold/issues/483
+        @var @set(Protected) public String protectedSet = "can only set in class and subclasses and other classes in the same package";
+        @var @set(Package) String packagePrivateSet = "can only set in class and other classes in the same package";
+        @var @set(Private) String privateSet = "can only set in class itself";
+
+        public Properties() {
+            protectedSet = "protected setter used in Properties";
+            packagePrivateSet = "package-private setter used in Properties";
+            privateSet = "private setter used in Properties";
+        }
+    }
+
+    public static class SubProperties extends Properties {
+        public SubProperties() {
+            protectedSet = "protected setter used in SubProperties";
+            packagePrivateSet = "package-private setter used in SubProperties";
+        }
+    }
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        System.out.println(properties);
+        SubProperties subProperties = new SubProperties();
+        System.out.println(subProperties);
+    }
+}
